bd12ce81080652b2c3008af4a989ed9670ad7053
Christopher <zwg_zero1@163.com>
2017-03-13 09:41:56 +0800

    change to support multiple items in 'database' in configure file

--- configure_fake.py
+++ configure_fake.py
@@ -1,65 +1,143 @@
 #! -*- coding: utf-8 -*-
-baidu_gaojing_fatal = {"service_id": "9999",
-                 "token": "d5cbe9b479b248e9c6a8",
+baidu_gaojing_fatal = {"service_id": "0000",
+                 "token": "8240ffe79b479b248e9c6a8",
                  }
-baidu_gaojing_info = {"service_id": "8888",
-                       "token": "37d027e07fff552",
+baidu_gaojing_info = {"service_id": "1111",
+                       "token": "307d439a02c79335897fff552",
                        }
 
 task = (
-    {'action': 'send_mail',
-     'mail_smtp': 'smtp.google.com',
-     'mail_login_user': 'ss_gaoj@gmail.com',
-     'mail_login_password': 'EI_F',
-     'mail_success_recipients': 'zuwe@bbb.net', # recipients should be in a string separated by comma
-     'mail_success_cc_recipients': 'zuwe@bbb.net',
+    {'execute_time': '1st every month 7:40',  # this is just for classify"
+     'action': 'send_mail',
+     'mail_smtp': 'smtp.mailserver.net',
+     'mail_login_user': 'mailloginname@mailserver.net',
+     'mail_login_password': 'password@youremail',
+     'mail_success_recipients': 'mailrecipient1@mailserver.net, mailrecipient2@mailserver.net', # recipients should be in a string separated by comma
+     'mail_success_cc_recipients': 'ccrecipient1@mailserver.net, ccrecipient2@mailserver.net',
      'mail_success_bcc_recipients': '', # same as above
-     'mail_fail_recipients': 'uo@edfdf.net',
-     'mail_success_sub': u'每日投资人数统计',
-     'mail_fail_sub': u'FAIL IN Daily  investor number report',
-     'mail_success_cont': u'每日投资人数统计',
-     'mail_fail_cont': u'FAIL IN Daily investor number report',
-     'attached_file': u'投资人数统计.xlsx',
-     'host':'10.13.2.125', 'port': 3306, 'user': 'root', 'passwd': 'test', 'db': 'ddddd',
-     'sql':
-         """
-            select
-            date(t3.dt_date) as '还款日期',
-            truncate(sum(t3.nb_principal),2) as '还款本金',
-            truncate(sum(t3.nb_interest),2) as '利息',
-            truncate(sum(t3.nb_principal+t3.nb_interest+nb_fee),2) as '还款本息',
-            count(*) as '笔数'
-            from fiz_repay_plan t3
-            left join fiz_loan t4 on t3.fk_loan_id =t4.pk_id
-            where t3.dc_status ='00'
-            and date(t3.dt_date) >= '2017-01-19' and date(t3.dt_date) <= '2017-02-18'
-            and t4.dc_category_code not in ('9','11','12','14','15','5')
-            group by 1
-            order by 1
-         """
+     'mail_fail_recipients': 'failrecipient1@mailserver.net, failrecipient2@mailserver.net',
+     'mail_success_sub': u'单标投资明细-月',
+     'mail_fail_sub': u'FAIL IN 单标投资明细-月',
+     'mail_success_cont': u'Dear xxxx，\n\n    关于2.0单标（按月）的结果已经发到邮箱，请您注意查收，谢谢。',
+     'mail_fail_cont': u'FAIL IN 单标投资明细-月',
+     'database': [
+         {
+             'local_path': u'/reports/单标投资明细-月',
+             'attached_file': u'loan_detail_month.xlsx',
+             'sheet_name': u'单标投资明细-月',
+             'host':'172.16.0.1', 'port': 3306, 'user': 'databaseuser', 'passwd': 'databasepassword', 'db': 'youdatabase',
+             'sql':
+                 """
+                 select
+                  '2.0' as '平台',
+                   t6.pk_id as '投资id',
+                   t7.vc_name as '单标名称',
+                   t7.nb_period as '锁定期',
+                   case t7.dc_period_type
+                   when  '00' then '按天算'
+                   when  '01' then '按月算'
+                   end as '计息方式（按月/按天)',
+                   concat(left(t7.nb_rate*100,4),'%') as '年化收益率',
+                   (t6.nb_amount) as '投资金额(元)',
+                   t3.vc_name as '投资人姓名',
+                     t3.vc_cellphone as '投资人手机号',
+                     t4.vc_value  as '投资人身份证号',
+                   t2.vc_name as '邀请人姓名',
+                  t2.vc_cellphone as '邀请人手机号',
+                     t5.vc_value as '邀请人身份证号',
+                   '单标' as '计划类型',
+                 """
+             },
+             # other items in database put here.
+         ],
+    },
+    {'execute_time': '1st every month 7:40',  # this is just for classify"
+     'action': 'send_mail',
+     'mail_smtp': 'smtp.server.net',
+     'mail_login_user': 'mailloginuser@mailserver.net',
+     'mail_login_password': 'passordofmaillogin',
+     'mail_success_recipients': 'recipient1@mailserver.net, recipient2@mailserver.net', # recipients should be in a string separated by comma
+     'mail_success_cc_recipients': 'ccmailrecipient1@mailserver.net',
+     'mail_success_bcc_recipients': '', # same as above
+     'mail_fail_recipients': 'failrecipient1@mailserver.net, failrecipient2@mailserver.net',
+     'mail_success_sub': u'后台投资明细-月',
+     'mail_fail_sub': u'FAIL IN 后台投资明细-月',
+     'mail_success_cont': u'Dear xxxx，\n\n    关于2.0投资明细（按月）的结果已经发到邮箱，请您注意查收，谢谢。',
+     'mail_fail_cont': u'FAIL IN 后台投资明细-月',
+     'database': [
+         {
+             'local_path': u'/reports/后台投资明细-月',
+             'attached_file': u'investment_details_month.xlsx',
+             'sheet_name': u'后台投资明细-月',
+             'host':'172.16.0.1', 'port': 3306, 'user': 'username', 'passwd': 'password', 'db': 'xxxx',
+             'sql':
+                 """
+                 select
+                 '2.0' as '平台',
+                 t4.pk_id as '投资id',
+                 t5.vc_name as '计划名称',
+                 t5.nb_period as '锁定期',
+                 case t5.dc_period_type
+                 when '00' then '按天'
+                 when '01' then '按月'
+                 end as '计息方式',
+                       concat(left(t5.nb_rate*100,4),'%') as '年化收益率',
+                      (t4.nb_amount) as '投资金额',
+                               t3.vc_name as '投资人姓名',
+                        t3.vc_cellphone as '投资人手机号',
+                        t6.vc_value as '投资人身份证号',
+                        t2.vc_name as '邀请人姓名',
+                        t2.vc_cellphone as '邀请人手机号',
+                        t7.vc_value  as '邀请人身份证号',
+                         t8.vc_name as '计划类型',
+                        t4.dt_datetime_start as '起息时间',
+                 order by 10
+                 """
+         },
+     ],
      },
-    {'action': 'upload_ftp',
-     'ftp_host': '10.13.4.56',
-     'ftp_user': 'ftpupload',
-     'ftp_password': 'ftppassword',
-     'attached_file': u'test_ftp_upload.xlsx',
-     'remote_path': u'投资人明细-理财计划',
-     'host':'10.13.2.125', 'port': 3306, 'user': 'root', 'passwd': 'test', 'db': 'database',
-     'sql':
-         """
-            select
-            date(t3.dt_date) as '还款日期',
-            truncate(sum(t3.nb_principal),2) as '还款本金',
-            truncate(sum(t3.nb_interest),2) as '利息',
-            truncate(sum(t3.nb_principal+t3.nb_interest+nb_fee),2) as '还款本息',
-            count(*) as '笔数'
-            from fiz_repay_plan t3
-            left join fiz_loan t4 on t3.fk_loan_id =t4.pk_id
-            where t3.dc_status ='00'
-            and date(t3.dt_date) >= '2017-01-19' and date(t3.dt_date) <= '2017-02-18'
-            and t4.dc_category_code not in ('9','11','12','14','15','5')
-            group by 1
-            order by 1
-         """
+     {'action': 'upload_ftp',
+      'ftp_host': '10.13.2.52',
+      'ftp_user': 'xxxxx',
+      'ftp_password': 'xxxxx',
+      'fail_cont': u'FAIL IN  战企月初ftp',
+      'mail_smtp': 'smtp.mailserver.net',
+      'mail_login_user': 'userlogin@mailserver.net',
+      'mail_login_password': 'password',
+      'mail_fail_recipients': 'mailrecipient1@mailserver, failrecipient2@mailserver.net',
+      'mail_fail_sub': u'FAIL IN 战企月初ftp',
+      'mail_fail_cont': u'FAIL IN 战企月初ftp',
+      'database': [
+          {
+             'local_path': u'/reports/交明细-单标',
+             'attached_file': u'Deal_Loan.xlsx',
+             'remote_path': u'成交明细-单标',
+             'sheet_name': u'成交明细-单标',
+             'host':'172.16.1.9', 'port': 3306, 'user': 'ername', 'passwd': 'password', 'db': 'database',
+             'sql':
+                 """
+                 select
+                u.pk_id as '用户id',u.vc_name as '投资人姓名',up.vc_value as '身份证号',u.vc_account as '投资人用户名',t.money as '成交金额',date(t.dtime) as '成交日期'
+                from
+                (SELECT i.nb_amount as 'money',date(i.dt_datetime) as 'dtime' ,i.pk_id
+                from fiz_invest i ,fiz_loan l
+                where i.fk_loan_id = l.pk_id
+                and l.dc_type in ('00','01')
+                and  i.dc_status in ('10','90','99')
+                and  i.dt_datetime >= '2015-05-11 00:00:00' and  i.dt_datetime <= now()
+                )as t
+                inner join fiz_invest i
+                on i.pk_id=t.pk_id
+                left join fiz_user u
+                on i.fk_user_id = u.pk_id
+                left join
+                (select * from fiz_user_prop
+                where vc_code = 'code-identity-number'
+                ) up
+                on u.pk_id = up.fk_user_id
+                 """
+          },
+          # other items of database come here
+     ],
      },
 )
--- query.py
+++ query.py
@@ -1,14 +1,15 @@
 # -*- coding: utf8 -*-
 # use python2.7 on linux amd64
 
-import configure  # configure.py is setting file of this program, format refer to configure_fake.py
+import configure_735_2_5weekdays as configure  # configure.py is setting file of this program, format refer to configure_fake.py
 import os
+import sys
 import time
 import json
 import requests
 import arrow
 import MySQLdb  # install mysqlclient please
-from xlwt import Workbook
+from xlsxwriter import Workbook
 import datetime
 import logging
 import ftplib
@@ -22,7 +23,7 @@ from email.header import Header
 
 # setup logging
 current_path = os.path.abspath(os.path.dirname(__file__))
-log_file = os.path.join(current_path, "messages.log")
+log_file = os.path.join(current_path, sys.argv[0] + ".log")
 logging.basicConfig(filename=log_file,
                     level=logging.INFO,
                     filemode='w',
@@ -38,10 +39,11 @@ def query_db(host, port, user, passwd, db, query_sql, charset="utf8"):
     c = db.cursor()
     c.execute(query_sql)
     # save column name
-    result.append([item[0] for item in c.description])
+    result.append([item[0].decode("utf-8") for item in c.description])  # must decode or python2.7 would treat as byte
     for item in c.fetchall():
         # convert returned datetime.datetime or datetime.date to string
         item_content = []
+        #print(item)
         for sub_item in item:
             if type(sub_item) == datetime.datetime:
                 sub_item = sub_item.strftime("%Y-%m-%d %H:%M:%S")
@@ -55,17 +57,25 @@ def query_db(host, port, user, passwd, db, query_sql, charset="utf8"):
 
 
 # write sql query result to excel file
-def write_xls(filename, content):
-    book = Workbook(encoding='utf-8')
-    sheet1 = book.add_sheet('Sheet 1')
+def write_xls(filename, sheetname, content):
+    #book = Workbook(filename, {'constant_memory': True})  # for debug to flush every row before new row begain write
+    book = Workbook(filename)
+    sheet1 = book.add_worksheet(sheetname)
     row_num = 0
     for row in content:
         col_num = 0
         for item in row:
+            # print item, type(item)
+            # try:
+            #     sheet1.write(row_num, col_num, item)
+            # except Exception as e:
+            #     print('有错误退出')
+            #     print(str(e))
+            #     exit()
             sheet1.write(row_num, col_num, item)
             col_num += 1
         row_num += 1
-    book.save(filename)
+    book.close()
 
 
 def send_mail(server, user, password,
@@ -74,7 +84,7 @@ def send_mail(server, user, password,
               bcc_recipients='',  # same as above
               subject='TEST',
               content='TEST',
-              attach_file=None,
+              attach_files=None,
               ):
     msg = MIMEMultipart()
     msg["subject"] = Header(subject, 'utf-8')
@@ -84,10 +94,10 @@ def send_mail(server, user, password,
         msg["Cc"] = cc_recipients
     msg.attach(MIMEText(content, 'plain', 'utf-8'))
 
-    if attach_file:
-        file_base_name = os.path.basename(attach_file)
+    for file_item in attach_files:
+        file_base_name = os.path.basename(file_item)
         attach_part = MIMEBase('application', 'octet-stream')
-        attach_part.set_payload(open(attach_file, 'rb').read())
+        attach_part.set_payload(open(file_item, 'rb').read())
         encoders.encode_base64(attach_part)
         attach_part.add_header('Content-Disposition', 'attachment',
                                filename=(Header(file_base_name, 'utf-8').encode()))
@@ -105,7 +115,7 @@ def send_mail(server, user, password,
         s.close()
         return True
     except Exception as e:
-        print(str(e) + "/nTry again after 3s.")
+        print(str(e) + "\nTry again after 3s.")
         time.sleep(3)
         try:
             s = smtplib.SMTP_SSL(server, timeout=30)
@@ -141,6 +151,12 @@ def send_ftp(host, user, password, local_file, remote_path):
     remote_file_name = os.path.basename(local_file)
     try:
         ftp = ftplib.FTP(host, user, password, timeout=3)
+        file_list = []
+        ftp.retrlines('LIST',file_list.append)
+        #print(file_list)
+        if remote_path.encode("gb2312") not in [item.split()[-1] for item in file_list]:
+            print("make fold")
+            ftp.mkd(remote_path.encode('gb2312'))
         ftp.cwd(remote_path.encode("gb2312"))
         # print(ftp.pwd().decode('gb2312'))
         file_handler = open(local_file, 'rb')
@@ -157,59 +173,71 @@ def send_ftp(host, user, password, local_file, remote_path):
 if __name__ == '__main__':
     for item in configure.task:
         content = ''
-        # try to execute db query, totally twice tried
-        try:
-            content = query_db(item['host'], item['port'], item['user'], item['passwd'],
-                               item['db'], item['sql'])
-        except Exception as e:
-            print(str(e) + '/nTry again.')
-            logging.error(str(e) + '/nTry again.')
-            time.sleep(3)
+        total_attached_files = []
+        for sql_item in item['database']:
+            # try to execute db query, totally twice tried
             try:
-                content = query_db(item['host'], item['port'], item['user'], item['passwd'],
-                                   item['db'], item['sql'])
+                content = query_db(sql_item['host'], sql_item['port'], sql_item['user'], sql_item['passwd'],
+                                   sql_item['db'], sql_item['sql'])
             except Exception as e:
-                print(str(e))
-                logging.error(str(e))
-                # try to send alert mail if db query fail
-                # if send_mail fail, send baidu gaojing
-                # if sending baidu gaojing fail do nothing
-                if send_mail(item['mail_smtp'], item['mail_login_user'], item['mail_login_password'],
-                             item['mail_fail_recipients'],
-                             subject=item['mail_fail_sub'],
-                             content=item['mail_fail_cont'],
-                             attach_file=log_file):
-                    exit()
-                else:
+                print(str(e) + '\nTry again.')
+                logging.error(str(e) + '\nTry again.')
+                time.sleep(3)
+                try:
+                    content = query_db(sql_item['host'], sql_item['port'], sql_item['user'], sql_item['passwd'],
+                                       sql_item['db'], sql_item['sql'])
+                except Exception as e:
+                    print(str(e))
+                    logging.error(str(e))
+                    # try to send alert mail if db query fail
+                    # if send_mail fail, send baidu gaojing
+                    # if sending baidu gaojing fail do nothing
+                    if send_mail(item['mail_smtp'], item['mail_login_user'], item['mail_login_password'],
+                                 item['mail_fail_recipients'],
+                                 subject=item['mail_fail_sub'],
+                                 content=item['mail_fail_cont'],
+                                 # attach_files should be a list or tuple.
+                                 attach_files=[log_file,]):
+                        exit()
+                    else:
+                        send_gaojing(configure.baidu_gaojing_info['service_id'], configure.baidu_gaojing_info['token'],
+                                     item['mail_fail_cont'])
+                        exit()
+            # the above all successful, write query result into excel file under current path
+            if not os.path.isdir(sql_item['local_path']):
+                os.mkdir(sql_item['local_path'])
+            file_to_write = os.path.join(sql_item['local_path'], sql_item['attached_file']) + \
+                            '_' + arrow.now().format('YYYYMMDD') + '.xlsx'
+            write_xls(file_to_write, sql_item['sheet_name'], content)
+            print("file: %s written finished" % file_to_write)
+            total_attached_files.append(file_to_write)
+            # if cation is "upload_ftp" upload earch file
+            if item['action'] == 'upload_ftp':
+                # two times to send file to ftp
+                if not send_ftp(item['ftp_host'], item['ftp_user'], item['ftp_password'], file_to_write,
+                                sql_item['remote_path']):
+                    time.sleep(3)
+                if not send_ftp(item['ftp_host'], item['ftp_user'], item['ftp_password'], file_to_write,
+                                sql_item['remote_path']):
                     send_gaojing(configure.baidu_gaojing_info['service_id'], configure.baidu_gaojing_info['token'],
-                                 item['mail_fail_cont'])
+                                 item['fail_cont'])
                     exit()
-        # the above all successful, write query result into excel file under current path
-        file_to_write = os.path.join(current_path, item['attached_file']) + '_' + arrow.now().format('YYYYMMDD') + \
-                        '.xlsx'
-        write_xls(file_to_write, content)
-        print("file: %s write" % file_to_write)
-        # read action from configure file, send_mail or upload file
+                logging.info("successful in upload file %s to ftp" % file_to_write)
+                print("successful in upload file %s to ftp" % file_to_write)
+
+        # if action is "send_mail", send all files in a task at one time
         if item['action'] == 'send_mail':
+            # in send_email function two times will try, so here just execute once.
             if not send_mail(item['mail_smtp'], item['mail_login_user'], item['mail_login_password'],
                              item['mail_success_recipients'],
                              cc_recipients=item['mail_success_cc_recipients'],
                              bcc_recipients=item['mail_success_bcc_recipients'],
                              subject=item['mail_success_sub'],
                              content=item['mail_success_cont'],
-                             attach_file=file_to_write):
+                             attach_files=total_attached_files):
                 send_gaojing(configure.baidu_gaojing_info['service_id'], configure.baidu_gaojing_info['token'],
                              item['mail_fail_cont'])
                 exit()
-            else:
-                logging.info("successful in send file %s using mail" % file_to_write)
-                print("successful in send file %s using mail" % file_to_write)
-        elif item['action'] == 'upload_ftp':
-            if not send_ftp(item['ftp_host'], item['ftp_user'], item['ftp_password'], file_to_write,
-                            item['remote_path']):
-                send_gaojing(configure.baidu_gaojing_info['service_id'], configure.baidu_gaojing_info['token'],
-                             item['mail_fail_cont'])
-                exit()
-            else:
-                logging.info("successful in upload file %s to ftp" % file_to_write)
-                print("successful in upload file %s to ftp" % file_to_write)
+            for item in total_attached_files:
+                logging.info("successful in send file %s using mail" % item)
+                print("successful in send file %s using mail" % item)
